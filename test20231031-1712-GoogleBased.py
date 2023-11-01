0f66ebbbcd03ccf52c6be3031f7b394190a478da
JP Professional <93789056+JohnPerrinPEng@users.noreply.github.com>
2023-11-01 11:09:48 -0700

    Save before change working diretory

--- test20231031-1712-GoogleBased.py
+++ test20231031-1712-GoogleBased.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import pandas as pd
+from googlesearch import search
+import time
 
 url = "https://www.google.ca/search"
 
@@ -8,7 +11,51 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36"
 }
 
-with open("20231031 1125  Canada Mining Company List 100 Only.csv","r") as file:
-    reader = csv.reader(file)
-    
-print(reader)
+# companies = []
+# with open("x.csv","r") as file:
+#     reader = csv.reader(file)
+#     for row in reader:x``
+#         # print(row)
+#         companies.append(row)
+# file.close()
+
+# companies = pd.read_csv('x.csv')
+# data = {'Company': companies}
+# df['Company'] = ''
+df = df.astype(str)
+df = pd.read_csv('20231031 1125  Canada Mining+ Company List.csv', on_bad_lines='skip',names=["Company","Site URL","Site Map"])
+
+print(df)
+
+# results = ['Company','URL','Site.xml']
+# for company in companies:
+#     # print("Company coming up next!")
+#     # print(company[0])
+#     results.append([company,getURL(company[0] ,"Canada Mining")])
+
+for index,row in df.iterrows():
+    df.at[index,'Site URL'] = getURL(row['Company'],'Canada Mining')
+    df.at[index,"Site Map"] = df.at[index,'Site URL'] + "sitemap.xml"
+    # sitexml = result[1] + "sitemap.xml"
+    # results[index][2]=sitexml
+
+df.to_csv('testoutput.csv', sep=',', index=True)
+# print("Results coming up next!")
+# print(results)
+
+# df['URL'] = ''
+
+# for index, row in df.iterrows():
+#     df.at[index, 'URL'] = getURL(row['Compan'], row['State'])
+#     time.sleep(2)
+
+# for company in reader:
+#     open("results.csv",'w',)
+
+def getURL(companyName, State):
+    try:
+        term = ' '.join([companyName, State])
+        for url in search(term, num_results=1):
+            return url
+    except:
+        return ''
---
+++ testcURL.py
@@ -0,0 +1,4 @@
+curl "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAwFBMVEX///+Ks46Ku4z8+f4AdwAAhwA+mEHE28Xswr/019by0M7uqqbMW0gAjxgMixIUjRgAmgZWuVlvwHJ8xX63Kx/WQzXTOirdST+kZTkhlTSUqZ0qb0AZjB0rqzAkrikXqR6c0p24Z2TTZ1/fz9DOVVCObTVVpFZmlnBAsEVajWrN587Dra6CbDAhmyUKqhLx+vu5PjZViW1ykoBWfGgDeyaSzpTHw8XESDXo7erOzdW7cnDIdnckaSK/wcgAWSXR49NY80YtAAAAfklEQVR4AZ3Igw4CAAAE0Lts2zamrP9f8/qAptyQbY7hDQf8jYJnw4WQZ+v7IePNQkkeBWPCxCeQbQHUFzOVCkrlFgoASbPVmnJpNOmYL54BiCy702grTfZJSxNneUugoHZJi460FzelcqWqFXs9mQSeanW1N93Au1JJiO+cABRAEp5VA3CBAAAAAElFTkSuQmCC" ^
+  -H "Referer: https://www.google.com/" ^
+  -H "User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Mobile Safari/537.36" ^
+  --compressed
